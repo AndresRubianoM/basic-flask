db399c7bc85a0ecf68e119e1494cb058054057d7
Andrés Rubiano <andresfrm2210@gmail.com>
2020-05-05 03:33:30 -0500

    signup add

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, session
 from flask_login import login_user, login_required, logout_user
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 #Local Imports
@@ -9,7 +9,7 @@ from . import auth
 #form class
 from ..forms import LoginForm
 #Conntection to the db
-from ..firestore_service import get_user
+from ..firestore_service import get_user, user_put
 #Classes for login user 
 from ..models import UserData, UserModel
 
@@ -32,8 +32,9 @@ def login():
 
         if user_doc.to_dict() is not None:
             password_from_db = user_doc.to_dict()['password']
-
-            if password == password_from_db:
+            
+            
+            if check_password_hash(password_from_db, password):
                 #user_data = UserData(username, password)
                 #user = UserModel(user_data)
                 user = UserModel.query(username)
@@ -53,8 +54,40 @@ def login():
 
     return render_template('login.html', **context)
 
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form': signup_form
+    }
 
-@auth.route('logout')
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+            login_user(user)
+            flash('Welcome!!!')
+
+            return redirect(url_for('hello'))
+        
+        else:
+            flash('That username already exists')
+
+
+
+    return render_template('signup.html', **context)
+
+
+
+@auth.route('/logout')
 @login_required
 def logout():
     logout_user()
--- app/firestore_service.py
+++ app/firestore_service.py
@@ -6,6 +6,8 @@ firebase_admin.initialize_app(credential, {'projectId': 'todos-flask'})
 
 db = firestore.client()
 
+#Functions that get info from the users in the db 
+
 def get_users():
     return db.collection('users').get()
 
@@ -14,3 +16,10 @@ def get_user(user_id):
 
 def get_todos(user_id): 
     return db.collection('users').document(user_id).collection('todos').get()
+
+#Functions to save the user's info in the db
+
+def user_put(user_data):
+    user_ref = db.collection('users').document(user_data.username)
+    user_ref.set({'password': user_data.password})
+    
